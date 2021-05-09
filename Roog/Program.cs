a4bd953884468c9206805329746d9593401b6ad3
Gabriella Govender <gabriella.govender@gmail.com>
2021-05-09 21:09:09 +0200

    MoreLines

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -7,6 +7,8 @@ namespace Roog
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Write new line");
+
         }
     }
 }
